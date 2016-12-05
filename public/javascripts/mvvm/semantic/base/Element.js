0d185c73ef973d694560b4a82a6ebb88ad96ae8d
scvzerng <842494771@qq.com>
2016-12-05 23:13:10 +0800

    动画库

--- public/javascripts/mvvm/semantic/base/Element.js
+++ public/javascripts/mvvm/semantic/base/Element.js
@@ -1,3 +1,20 @@
 /**
+ * create vue element
  * Created by Administrator on 2016/12/5.
  */
+
+
+Vue.component("s-div",{
+    props:['class','style','events','child','tag'],
+    template:'<div class="ui container" :class="[buttonClass]" tabindex="0" style="buttonStyle"></div>',
+    data:function(){
+
+        return {
+            buttonStyle:$.extend(true,{},this.style,{
+
+            }),
+            buttonClass:this.class,
+
+        }
+    }
+});
