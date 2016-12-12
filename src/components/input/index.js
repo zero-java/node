caba3a31231aab276f357621295195eb9136e655
scvzerng <842494771@qq.com>
2016-12-12 18:19:21 +0800

    input

---
+++ src/components/input/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+
+import image from "./input.vue"
+
+export default image;
---
+++ src/components/input/input.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="ui input" :class="classes" :style="styles">
+        <slot></slot>
+    </div>
+    <img  :class="classes" :style="styles"/>
+</template>
+<script>
+    export default{
+        name:'SeInput',
+        props:{
+            classes:Object,
+            styles:Object
+        },
+        data(){
+            return{
+
+            }
+        },
+    }
+</script>
