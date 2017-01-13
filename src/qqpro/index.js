4f765fe2bb172accdadb36deecc579e2384eb2a4
scvzerng <842494771@qq.com>
2017-01-13 14:05:14 +0800

    过渡动画

--- src/qqpro/index.js
+++ src/qqpro/index.js
@@ -4,4 +4,6 @@
 import body from "./body/body.vue"
 import nav from "./nav/navbar.vue"
 import table from "./body/background/index";
-export default [body,nav,table]
+import news from "./body/news.vue";
+
+export default [body,nav,table,news]
